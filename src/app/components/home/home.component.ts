962951599bd6030adc741fed7d824485ecc568c0
priyanka jain <priyanka.jain@yash.com>
2025-02-20 20:15:35 +0530

    commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DrawerModule } from 'primeng/drawer';
+import { SelectButtonModule } from 'primeng/selectbutton';
+
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule],
+  imports: [ButtonModule,CardModule,DrawerModule,SelectButtonModule,FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  visible2: boolean = false;
+  stateOptions: any[] = [{ label: 'Notifications', value: 'notifications' }, { label: 'Trainiing Links', value: 'trainiingLinks' }];
 
+  value: string = 'notifications';
+  
 }
